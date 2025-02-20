68150fbe50a9b174ade8955cac9e337a14b6b803
xujunping <23@>
2025-02-20 18:33:59 +0800

    修改代码

--- geth/13/13geth.go
+++ geth/13/13geth.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"fmt"
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -18,12 +17,12 @@ import (
 // 以太坊代币交易
 func main() {
 
-	client, err := ethclient.Dial("https://eth-holesky.g.alchemy.com/v2/week-63a2htFcK69secsJ8zxnFXAx8_1")
+	client, err := ethclient.Dial("https://eth-sepolia.g.alchemy.com/v2/")
 	if err != nil {
 		log.Fatal(err)
 	}
 	// 加载私钥
-	privateKey, err := crypto.HexToECDSA("ec68ae6b9c67ee944ef3f3256c02397caa92649db47593b5e3494c6d95a9ea61")
+	privateKey, err := crypto.HexToECDSA("")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,6 +31,7 @@ func main() {
 	if !ok {
 		log.Fatal("error casting public key to ECDSA")
 	}
+	// 发送地址
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 	// 获取随机数
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
@@ -43,7 +43,7 @@ func main() {
 	// 获取平均燃气价格
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	// 代币合约地址
-	tokenAddress := common.HexToAddress("0x7B8D92EAbAfAB1d6aE41401Cc491a5a7013454d1")
+	tokenAddress := common.HexToAddress("0x8FD801A567d963AdA02D57Ae0429f3B80D0143A3")
 	// erc20 转账函数
 	transferFnSignature := []byte("transfer(address,uint256)")
 	// 生成函数签名
@@ -51,8 +51,9 @@ func main() {
 	hash.Write(transferFnSignature)
 	// 取前面 4 个字节
 	methodId := hash.Sum(nil)[:4]
-	// 发送代码的地址 发送代币地址左填充到32位
-	toAddress := common.HexToAddress("0x1AFE60C3631568541A34bfe66f6d3bc59B28D3fF")
+	fmt.Println(hexutil.Encode(methodId))
+	// 接收地址 发送代币地址左填充到32位
+	toAddress := common.HexToAddress("0xe031623C8C15359F832cC4A8956e37C05aBd8ecB")
 	paddedAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
 	fmt.Println(hexutil.Encode(paddedAddress))
 	// 发送多少代币
@@ -68,16 +69,17 @@ func main() {
 	data = append(data, paddedAmount...)
 
 	// 估算 gas 费用
-	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
-		To:   &toAddress,
-		Data: data,
-	})
+	//gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+	//	To:   &toAddress,
+	//	Data: data,
+	//})
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(gasLimit)
+	//	fmt.Println(gasLimit)
 
+	gasLimit := uint64(100000)
 	// 构建交易 to 为只能合约地址
 	tx := types.NewTransaction(nonce, tokenAddress, value, gasLimit, gasPrice, data)
 
